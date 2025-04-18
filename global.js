640d19d8257358d52b7e76e0c9305b14cd9449f2
Christine Law <christine.law14013@gmail.com>
2025-04-17 23:25:17 -0700

    update js

--- global.js
+++ global.js
@@ -3,3 +3,9 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const navLinks = $$("nav a");
+console.log(navLinks);
+const currentLink = Array.from(navLinks).find(
+(a) => a.host === location.host && a.pathname === location.pathname);
+currentLink?.classList.add('current');
