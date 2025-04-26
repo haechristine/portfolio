58eb56e0782a298f2aab5d6cab75662b111ae6d6
Christine Law <christine.law14013@gmail.com>
2025-04-25 23:53:44 -0700

    updat home

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
-// const projects = await fetchJSON('./lib/projects.json');
-// const latestProjects = projects.slice(0, 3);
+import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
 
-// const projectsContainer = document.querySelector('.projects');
+const projectsContainer = document.querySelector('.projects');
 
-// renderProjects(latestProjects, projectsContainer, 'h2');
+renderProjects(latestProjects, projectsContainer, 'h2');
 
