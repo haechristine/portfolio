8c10ee492172e5e12bd33a974e29647a31948e6f
Christine Law <christine.law14013@gmail.com>
2025-04-25 23:16:16 -0700

    project count

--- index.js
+++ index.js
@@ -4,11 +4,5 @@ const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
 
-const titleElement = document.querySelector('.projects-title');
-
-if (titleElement) {
-    titleElement.textContent = `${projects.length} Projects`; 
-}
-
 renderProjects(latestProjects, projectsContainer, 'h2');
 
--- projects/projects.js
+++ projects/projects.js
@@ -1,9 +1,20 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
-async function loadProjects() {
-    const projects = await fetchJSON('../lib/projects.json');
-    const projectsContainer = document.querySelector('.projects');
-    renderProjects(projects, projectsContainer, 'h2');
+const projects = await fetchJSON('./lib/projects.json');
+const projectsContainer = document.querySelector('.projects');
+
+const titleElement = document.querySelector('.projects-title');
+
+if (titleElement) {
+    titleElement.textContent = `${projects.length} Projects`; 
 }
+
+renderProjects(latestProjects, projectsContainer, 'h2');
+
+// async function loadProjects() {
+//     const projects = await fetchJSON('../lib/projects.json');
+//     const projectsContainer = document.querySelector('.projects');
+//     renderProjects(projects, projectsContainer, 'h2');
+// }
 
 loadProjects();
