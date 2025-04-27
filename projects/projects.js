49e16c375b0b4055850fd67d1b84171cd634f48f
Christine Law <christine.law14013@gmail.com>
2025-04-26 17:22:00 -0700

    github stats update

--- projects/projects.js
+++ projects/projects.js
@@ -10,28 +10,3 @@ if (titleElement) {
 
 renderProjects(projects, projectsContainer, 'h2');
 
-// import { fetchJSON, renderProjects } from '../global.js';
-
-// console.log('Running projects.js');  // Add this to check if the script runs
-
-// const projects = await fetchJSON('./lib/projects.json');
-// console.log('Fetched projects:', projects);  // Check if projects are fetched correctly
-
-// const projectsContainer = document.querySelector('.projects');
-
-// // Render the projects if the data is valid
-// if (projects && projects.length > 0) {
-//     renderProjects(projects, projectsContainer, 'h2');
-// } else {
-//     console.error('No projects found or failed to load the data.');
-//     projectsContainer.innerHTML = '<p>No projects available.</p>';
-// }
-
-
-// async function loadProjects() {
-//     const projects = await fetchJSON('../lib/projects.json');
-//     const projectsContainer = document.querySelector('.projects');
-//     renderProjects(projects, projectsContainer, 'h2');
-// }
-
-// loadProjects();
