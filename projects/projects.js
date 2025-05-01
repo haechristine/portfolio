aadfcd6e0505ee29be76d596ad62e878d56b617a
Christine Law <christine.law14013@gmail.com>
2025-04-30 20:14:54 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -12,8 +12,8 @@ renderProjects(projects, projectsContainer, 'h2');
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-let data = [1, 2];
-let colors = ['gold', 'purple'];
+let data = [1, 2, 3, 4, 5, 5];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 let sliceGenerator = d3.pie();
 let arcData = sliceGenerator(data);
