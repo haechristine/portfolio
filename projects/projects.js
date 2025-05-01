55da3f89ba7a0a0dd985fa9725d7667b7e306046
Christine Law <christine.law14013@gmail.com>
2025-04-30 19:30:07 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -10,3 +10,8 @@ if (titleElement) {
 
 renderProjects(projects, projectsContainer, 'h2');
 
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });
